d89e595d9ab8cc86fc37bfd2b873eac16853a331
Akib Mashrur <akibmashrur@gmail.com>
2023-01-02 15:05:07 +1100

    added video streaming function

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,31 @@
-def test(msg="Hello World"):
-    print(msg)
 
+def processVideoStream():
+    """Opens a video stream and converts each frame to black and white."""
+    import cv2
+
+    # Open a video stream
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read a frame from the video stream
+        ret, frame = cap.read()
+
+        # Convert the frame to black and white
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the frame
+        cv2.imshow("frame", gray)
+
+        # Wait for a key press
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # Release the video stream
+    cap.release()
+    cv2.destroyAllWindows()
 
 def main():
-    test()
+    processVideoStream()
 
 
 if __name__ == "__main__":
